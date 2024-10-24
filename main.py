89c0a68fb2b53fc111c2b7d34d27ee90b285aa9e
afgigot19 <afgigot@gmail.com>
2024-10-23 22:02:08 -0400

    test comment

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ def decode(code):
     pass
 
 def encode(initial):
-    global code = ""
+    global code
+    code=""
     for i in range(0,len(initial)):
         code += str((int(initial[i])+3)%10)
     print("Your password has been encoded and stored!")        
 
 def main():
     while True:
-	print("Menu")
+        print("Menu")
         print("-------------")
         print("1. Encode")
         print("2. Decode")
@@ -19,12 +20,14 @@ def main():
         print("")
         option = input("Please enter an option: ")
         if option=="1":
-	    initial = ("Please enter your password to encode: ")
+            initial = input("Please enter your password to encode: ")
             encode(initial)
-	if option=="2":
-	    print(f"The encoded password is {decode(code)}, and the original password is {initial}"
-	if option=="3":
-	    break
+            print("")
+        if option=="2":
+            print(f"The encoded password is {code}, and the original password is {decode(code)}")
+            print("")
+        if option=="3":
+            break
 
 if __name__ == "__main__":
     main()
